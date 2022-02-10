579d40bb3969a6c5dbf791128b81fd33d987f246
StaticCloud <diegomarrs@gmail.com>
2022-02-09 20:24:55 -0800

    added post model

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {};
+
+// init model, which is a table in our database
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // text content of post, must be at least 100 characters
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [100]
+            }
+        },
+        // reference user model through it's id
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        // mandatory sequelize connection
+        sequelize,
+        // pluralize table name when getting posts
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
