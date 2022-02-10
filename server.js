06ef1cfd5d52cc8f514f3c1e8015ff1bfd9f17de
StaticCloud <diegomarrs@gmail.com>
2022-02-09 19:52:20 -0800

    added mysql db

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+// require express module
 const express = require('express');
 
+// get sequelize connection from config/connection
+const sequelize = require('./config/connection');
+
+// create new express app
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log('Now Listening'))
+// sync the database
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'))
+})
