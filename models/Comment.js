7493adc22a8a641463a88c662ad49af0deefbe91
StaticCloud <diegomarrs@gmail.com>
2022-02-09 20:32:44 -0800

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [10]
+            }
+        },
+        // establish user relationship via id
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        // establish post relationship via id
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        // mandatory sequelize connection
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+)
+
+module.exports = Comment;
