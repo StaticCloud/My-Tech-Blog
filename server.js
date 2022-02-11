a256a414dfa90bff40a611ea98c1dd48f9ec332f
StaticCloud <diegomarrs@gmail.com>
2022-02-11 15:33:52 -0800

    added sessions

--- server.js
+++ server.js
@@ -12,10 +12,27 @@ const hbs = exphbs.create();
 
 const path = require('path');
 
+// require session modules
+const session = require('express-session');
+// import this so we can save session data to our database
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+
 // create new express app
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+// session options
+const sess = {
+    secret: process.env.SESS_KEY,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    // where to store our session
+    store: new SequelizeStore({ db: sequelize })
+}
 
 // only looks at requests where the content header matches the type
 // returns middleware that parses json
@@ -25,6 +42,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(routes);
 
