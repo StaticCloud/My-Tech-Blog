530f6de7fe71e9e0930505d4cc2bb78f6b7c8346
StaticCloud <diegomarrs@gmail.com>
2022-02-12 20:34:04 -0800

    added page for creating posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -22,7 +22,17 @@ router.get('/', auth, async (req, res) => {
                 ]
             }
         )
-        res.render('dashboard', { posts, loggedIn: true });
+
+        res.render('dashboard', { posts, username: req.session.username, loggedIn: true });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.get('/create', auth, async (req, res) => {
+    try {
+        res.render('new-post', { loggedIn: true });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
