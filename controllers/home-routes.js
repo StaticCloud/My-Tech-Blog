980d533eed7581f91c9766f27bdbab82eabc1c42
StaticCloud <diegomarrs@gmail.com>
2022-02-13 22:10:28 -0800

    added page for single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,14 @@
 // create express router for handling endpoints
 const router = require('express').Router();
 
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 // home route
 router.get('/', async (req, res) => {
     try {
         let posts = await Post.findAll(
             {
-                attributes: ['title', 'content'],
+                attributes: ['id', 'title', 'content'],
                 include: [
                     {
                         model: User,
@@ -20,10 +20,45 @@ router.get('/', async (req, res) => {
         
         posts = posts.map(post => post.get({ plain: true }));
 
-        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn, singlePost: true });
         
     } catch (err) {
         // server error
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        let post = await Post.findByPk(req.params.id,
+            {
+                attributes: ['id', 'title', 'content'],
+                order: [['created_at', 'DESC']],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username']
+                    },
+                    {
+                        model: Comment,
+                        attributes: ['text'],
+                        include: [
+                            {
+                                model: User,
+                                attributes: ['username']
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+
+        post = post.get({ plain: true });
+
+        res.render('single-post', { post, loggedIn: req.session.loggedIn, singlePost: false });
+    } catch (err) {
+        // return server error message
         console.log(err);
         res.status(500).json(err);
     }
