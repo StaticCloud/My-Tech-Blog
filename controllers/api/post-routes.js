5656c11e9402006810495a8da37d69fd9acbb171
StaticCloud <diegomarrs@gmail.com>
2022-02-13 23:42:12 -0800

    users may now delete posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -102,7 +102,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const response = await User.destroy(
+        const response = await Post.destroy(
             {
                 where: { 
                     id: req.params.id 
