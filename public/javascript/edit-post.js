e20ec95909760b0a523246577aea3f65d37a8ba6
StaticCloud <diegomarrs@gmail.com>
2022-02-14 21:31:08 -0800

    users may now edit posts

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,3 +1,28 @@
+editPost = async event => {
+    event.preventDefault();
+
+    const content = document.querySelector('#content').value.trim();
+
+    // get the id of the post
+    const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify(
+            {
+                content
+            }
+        ),
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    if (response.ok) {
+        document.location.replace(`/posts/${post_id}`)
+    } else {
+        alert(response.statusText);
+    }
+}
+
 deletePost = async event => {
     event.preventDefault();
 
@@ -15,4 +40,5 @@ deletePost = async event => {
     }
 }
 
+document.querySelector('.save-post').addEventListener('click', editPost);
 document.querySelector('.delete-post').addEventListener('click', deletePost);
