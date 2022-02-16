b88f72317153ba5846f379338cf9a800b55b10c3
StaticCloud <diegomarrs@gmail.com>
2022-02-15 18:06:49 -0800

    fixed typo in connection

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create a new sequelize connection to a database depending on the environment
-let sequelize = (process.env.JAWS_DB) ? new Sequelize(process.env.JAWS_DB) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize = (process.env.JAWSDB_URL) ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
